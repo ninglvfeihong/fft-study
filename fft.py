dbcc13388ae5b6aa11fc7ffc74fd2b6dda4467ad
ninglvfeihong <ninglvfeihong@gmail.com>
2017-01-16 12:41:46 +0000

    Test iff, sinc -> rect

--- fft.py
+++ fft.py
@@ -21,14 +21,6 @@ def mirr(y):
         o.append(y[i]+y[len(y)-i])
     return o
 
-def mrr(y):
-    o=[]
-    for i in range(len(y)):
-        if(i<len(y)/2):
-            o.append(y[i]);
-        else:
-            o.append(y[len(y)-i]);
-    return o
 
 def rect(x):
     return list(rect_(x))
@@ -38,24 +30,26 @@ N = 8000
 # sample spacing
 T = 1.0 / 800.0
 x = np.linspace(0.0, N*T, N)
-y = rect(x*8.1)
-yf = scipy.fftpack.fft(y)
-xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
-#time domain
+xf = np.linspace(0.0,800.0,N)
+yf = mirr(np.sinc(xf))
+
+y=ifft(yf);
+
+#frequence domain
 fig2d=plt.figure()
 ax2 = fig2d.add_subplot(111);
-ax2.plot(x,y);
+ax2.plot(xf,yf);
 fig2d.show();
 
-#frequence domain just real part
+#time domian just real part
 fig2df=plt.figure()
 ax2f = fig2df.add_subplot(111);
-ax2f.plot(x,fft(y));
+ax2f.plot(x,y);
 fig2df.show();
 
-#frequence domain display real and imag part
+#time domain display real and imag part
 fig3df=plt.figure()
 ax3f = fig3df.add_subplot(111,projection="3d");
-ax3f.plot(x,np.imag(fft(y)),np.real(fft(y)));
+ax3f.plot(x,np.imag(y),np.real(y));
 fig3df.show();
