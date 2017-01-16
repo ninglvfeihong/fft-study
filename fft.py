b38a8f6748e89ef5a2886af46355a20ecbff9fd6
ninglvfeihong <ninglvfeihong@gmail.com>
2017-01-16 12:14:04 +0000

    test 3D plot fft transform for both real and imag part of the complex number

--- fft.py
+++ fft.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import scipy.fftpack
 from scipy.fftpack import fft
 from scipy.fftpack import ifft
@@ -41,18 +42,20 @@ y = rect(x*8.1)
 yf = scipy.fftpack.fft(y)
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
+#time domain
 fig2d=plt.figure()
 ax2 = fig2d.add_subplot(111);
 ax2.plot(x,y);
-ax2.plot(x,rect(8.1*(x-1)))
-ax2.plot(x,rect(8.1*(x-2)))
-ax2.plot(x,rect(8.1*(x-3)))
 fig2d.show();
 
+#frequence domain just real part
 fig2df=plt.figure()
 ax2f = fig2df.add_subplot(111);
 ax2f.plot(x,fft(y));
-ax2f.plot(x,fft(rect(8.1*(x-1))))
-ax2f.plot(x,fft(rect(8.1*(x-2))))
-ax2f.plot(x,fft(rect(8.1*(x-3))))
 fig2df.show();
+
+#frequence domain display real and imag part
+fig3df=plt.figure()
+ax3f = fig3df.add_subplot(111,projection="3d");
+ax3f.plot(x,np.imag(fft(y)),np.real(fft(y)));
+fig3df.show();
