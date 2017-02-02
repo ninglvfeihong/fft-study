208c56bb1e0f7de156d9998d77d7ce568cf71e89
ninglvfeihong <ninglvfeihong@gmail.com>
2017-02-02 12:00:57 +0000

    Test ZigBee half-sine pulse shaping response in frequency domain

---
+++ test ZigBee haf-sine pulse shaping response.py
@@ -0,0 +1,52 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import scipy.fftpack
+from scipy.fftpack import fft
+from scipy.fftpack import ifft
+
+def rect_(x):
+    for i in x:
+        if(i<-0.5):
+            yield 0
+        elif(i < 0.5):
+            yield 1
+        else:
+            yield 0
+
+def mirr(y):
+    o=[y[0]];
+    for i in range(1,len(y)):
+        o.append(y[i]+y[len(y)-i])
+    return o
+
+
+def rect(x):
+    return list(rect_(x))
+
+
+x = np.linspace(0.0, 100, 10000)
+y = mirr(np.cos(np.pi*x)*rect(x))
+yf = fft(y)
+xf = x
+
+
+#frequence domain
+fig2d=plt.figure()
+ax2 = fig2d.add_subplot(111);
+ax2.plot(xf,yf);
+fig2d.show();
+
+#time domian just real part
+fig2df=plt.figure()
+ax2f = fig2df.add_subplot(111);
+ax2f.plot(x,np.real(y));
+fig2df.show();
+
+#time domain display real and imag part
+#fig3df=plt.figure()
+#ax3f = fig3df.add_subplot(111,projection="3d");
+#ax3f.plot(x,np.imag(y),np.real(y));
+#fig3df.show();
