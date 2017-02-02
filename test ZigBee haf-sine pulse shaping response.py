ef3da53fa3f7b1707feda7b7b054b1eab49fc92c
ninglvfeihong <ninglvfeihong@gmail.com>
2017-02-02 14:40:34 +0000

    Simulate sending data and compare in situation With or without half-sine pulse shaping

--- test ZigBee haf-sine pulse shaping response.py
+++ test ZigBee haf-sine pulse shaping response.py
@@ -31,31 +31,37 @@ N=10000;
 
 x = np.linspace(0.0, N*T, N)
 y = 0.0*x;
+y2 = 0.0*x;
 for i in range(0,int(N*T)):
     if np.random.rand()<0.5 :
         y = y + np.sin(np.pi*(x-i))*rect(x-i-0.5)
+        y2 = y2 + rect(x-i-0.5)
     else:
         y = y - np.sin(np.pi*(x-i))*rect(x-i-0.5)
-
+        y2 = y2 - rect(x-i-0.5)
 
 yf = fft(y)
+yf2 = fft(y2)
 xf = np.linspace(0.0, 1.0/T,N)
 
 #yf = yf*rect(x/4)+yf*rect((x-100)/4)
 #y=ifft(yf);
 yf = np.log10(np.abs(yf)**2)*10
+yf2 = np.log10(np.abs(yf2)**2)*10
 
 
 #frequence domain
 fig2d=plt.figure()
 ax2 = fig2d.add_subplot(111);
 ax2.plot(xf,yf);
+ax2.plot(xf,yf2);
 fig2d.show();
 
 #time domian just real part
 fig2df=plt.figure()
 ax2f = fig2df.add_subplot(111);
 ax2f.plot(x,np.real(y));
+ax2f.plot(x,np.real(y2));
 fig2df.show();
 
 #time domain display real and imag part
