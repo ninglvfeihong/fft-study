62c6df29fcbff45e83f972147dfdec0005f97276
ninglvfeihong <ninglvfeihong@gmail.com>
2017-02-02 15:33:35 +0000

    wave shifting in time domain does not change energy distribution in Frequency domain. but the Real and Imag part in Frequency domain will be largely changed (Just like mixed or multiply with sine or cosine wave in frequency domain).
     Changes to be committed:
    	new file:   test ZigBee haf-sine pulse shaping response 3.png
    	new file:   test ZigBee haf-sine pulse shaping response 4.png
    	modified:   test ZigBee haf-sine pulse shaping response.py

--- test ZigBee haf-sine pulse shaping response.py
+++ test ZigBee haf-sine pulse shaping response.py
@@ -32,12 +32,12 @@ N=10000;
 x = np.linspace(0.0, N*T, N)
 y = 0.0*x;
 y2 = 0.0*x;
-for i in range(0,int(N*T)):
-    if np.random.rand()<0.5 :
-        y = y + np.sin(np.pi*(x-i))*rect(x-i-0.5)
+for i in range(1,2):
+    if np.random.rand()< 0.5 :
+        y = y + rect(x-i-0.5 -5)
         y2 = y2 + rect(x-i-0.5)
     else:
-        y = y - np.sin(np.pi*(x-i))*rect(x-i-0.5)
+        y = y - rect(x-i-0.5-5)
         y2 = y2 - rect(x-i-0.5)
 
 yf = fft(y)
@@ -45,16 +45,18 @@ yf2 = fft(y2)
 xf = np.linspace(0.0, 1.0/T,N)
 
 #yf = yf*rect(x/4)+yf*rect((x-100)/4)
-#y=ifft(yf);
-yf = np.log10(np.abs(yf)**2)*10
-yf2 = np.log10(np.abs(yf2)**2)*10
+yfl = np.log10(np.abs(yf)**2)*10
+yfl2 = np.log10(np.abs(yf2)**2)*10
 
 
 #frequence domain
 fig2d=plt.figure()
-ax2 = fig2d.add_subplot(111);
+ax2 = fig2d.add_subplot(211);
 ax2.plot(xf,yf);
 ax2.plot(xf,yf2);
+ax2 = fig2d.add_subplot(212);
+ax2.plot(xf,yfl);
+ax2.plot(xf,yfl2);
 fig2d.show();
 
 #time domian just real part
@@ -66,7 +68,8 @@ ax2f.plot(x,np.real(y2));
 fig2df.show();
 
 #time domain display real and imag part
-#fig3df=plt.figure()
-#ax3f = fig3df.add_subplot(111,projection="3d");
-#ax3f.plot(x,np.imag(y),np.real(y));
-#fig3df.show();
+fig3df=plt.figure()
+ax3f = fig3df.add_subplot(111,projection="3d");
+ax3f.plot(xf,np.imag(yf),np.real(yf));
+ax3f.plot(xf,np.imag(yf2),np.real(yf2));
+fig3df.show();
