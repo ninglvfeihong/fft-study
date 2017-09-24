0a199fb930e26b420f6b9bc788b23ca60ff875aa
Xiao WANG <ninglvfeihong@gmail.com>
2017-09-24 23:00:02 +0100

    Generate video

--- ANT-ARRAY/ANT-ARRAY.py
+++ ANT-ARRAY/ANT-ARRAY.py
@@ -4,15 +4,18 @@ import math
 import cv2
 import os
 
-image_folder = 'series'
 video_name = 'video.mp4'
-if not os.path.exists(video_name):
-    os.makedirs(video_name)
+tmp_img ='temp_image.png'
 
 #circle1 = plt.Circle((0, 0), 1, color='black',linewidth=1,fill=False)
 fig, ax = plt.subplots() # note we must use plt.subplots, not plt.subplot
 
-
+#video
+fig.set_size_inches(9, 9)
+fig.savefig(tmp_img)
+frame = cv2.imread(tmp_img)
+height, width, layers = frame.shape
+video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'XVID'), 24, (width,height))
 
 #strangth one point SP;
 SP_L=[];
@@ -20,9 +23,9 @@ for i in range (100):
     SP_L.append((i,i**2/100))
 
 for i in range (100):
-    SP_L.append((i,100-i**2/60)
+    SP_L.append((i,100-i**2/60))
 
-index=0;   
+index=0;
 for SP in SP_L:
     c = plt.Circle(SP, 1, color='red')
     ax.add_artist(c)
@@ -44,18 +47,11 @@ for SP in SP_L:
     fig.set_size_inches(9, 9)
     #plt.show()
     index=index+1;
-    fig.savefig('series/' + str(index).zfill(5)+'.png')
+    fig.savefig(tmp_img)
     ax.clear();
-
-
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
-
-video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'XVID'), 24, (width,height))
-
-for image in images:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
-
+    video.write(cv2.imread(tmp_img))
+    print("processing frame:"+str(index)+"/"+str(len(SP_L)));
+	
 cv2.destroyAllWindows()
 video.release()
+os.remove(tmp_img)
